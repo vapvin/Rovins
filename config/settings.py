4a6ad7f8684667c804cad983f3b7f3879f856134
vapvin <tpehdj12@daum.net>
2020-02-21 17:09:40 +0900

    Create Room Models Connected User Models

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,7 @@ PROJECT_APPS = [
     "rooms.apps.RoomsConfig",
 ]
 
-THIRD_PARTY_APPS = []
+THIRD_PARTY_APPS = ["django-countries"]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 
--- core/models.py
+++ core/models.py
@@ -5,8 +5,8 @@ class TimeStampedModel(models.Model):
 
     """ Time Stamped Model """
 
-    created = models.DateTimeField
-    update = models.DateTimeField
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
