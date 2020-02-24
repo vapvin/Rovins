0a4fa2a6d13189381fc972d480f863c3bdd54858
vapvin <tpehdj12@daum.net>
2020-02-24 15:20:06 +0900

    Room Admin Editing

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,14 +2,25 @@ from django.contrib import admin
 from . import models
 
 
-@admin.register(models.RoomType)
+@admin.register(models.RoomType, models.Amenity, models.Facility, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
+
+    """ Item Admin Definition """
+
     pass
 
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
-    """ Custom Room Admin """
+    """ Room Admin Definition """
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoADmin(admin.ModelAdmin):
+
+    """ Photo Admin Definition """
 
     pass
