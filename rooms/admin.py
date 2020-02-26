adc9d4ece21a181f806c5b83474c72a87ec50b0a
vapvin <tpehdj12@daum.net>
2020-02-27 00:03:10 +0900

    Room Admin Panner Customizing

--- rooms/admin.py
+++ rooms/admin.py
@@ -15,6 +15,35 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address","price")}
+        ),
+        (
+            "Times",
+            {"classes":("collapse",),"fields": ("check_in","check_out","instant_book")}
+        ),
+        (
+            "Spaces",
+            {"classes":("collapse",),
+             "fields": (
+                "guests",
+                "beds",
+                "bedrooms",
+                "baths",
+            )}
+        ),
+        (
+            "More About the Space",
+            {"classes":("collapse",),"fields": ("amenties","facilities","house_rules",)}
+        ),
+        (
+            "Last Details",
+            {"classes":("collapse",),"fields": ("host",)}
+        ),
+    )
+
     list_display = (
         "name",
         "country",
@@ -30,9 +59,24 @@ class RoomAdmin(admin.ModelAdmin):
         "instant_book"
     )
 
-    list_filter = ("instant_book", "city", "country")
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenties",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
 
     search_fields = ("^city","^host__username",)
+
+    filter_horizontal = (
+        "amenties",
+        "facilities",
+        "house_rules",
+    )
 
 
 @admin.register(models.Photo)
