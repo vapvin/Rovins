8454539ca2e1cde5cdd8fcbd148bf30f221b625c
vapvin <tpehdj12@daum.net>
2020-03-18 12:02:37 +0900

    Error Test

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
-
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
