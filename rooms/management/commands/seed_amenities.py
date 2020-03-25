eafaf7186609c45d8fcee277314d3526c468366f
vapvin <tpehdj12@daum.net>
2020-03-25 14:35:11 +0900

    Handle Method Error Fix

--- rooms/management/commands/seed_amenities.py
+++ rooms/management/commands/seed_amenities.py
@@ -1,15 +1,18 @@
 from django.core.management.base import BaseCommand
 from rooms.models import Amenity
 
+
 class Command(BaseCommand):
 
     help = "This command tells me that he loves me"
 
-    def add_arguments(self, parser):
-        """ parser.add_argument(
+    """     def add_arguments(self, parser):
+        parser.add_argument(
             "--times", help="How many times do you want me to tell you that I love you?"
-        ) """
+        )
+        """
 
+    def handle(self, *args, **options):
         amenities = [
             "Air conditioning",
             "Alarm Clock",
@@ -56,5 +59,4 @@ class Command(BaseCommand):
         ]
         for a in amenities:
             Amenity.objects.create(name=a)
-        self.stdout.write(self.style.SUCCESS("Amenities Create!"))
-
+        self.stdout.write(self.style.SUCCESS("Amenities created!"))
