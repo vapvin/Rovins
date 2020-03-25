53eac186578d0722366a75828de8e493fe0d8fb6
vapvin <tpehdj12@daum.net>
2020-03-25 13:26:00 +0900

    Amenities List Create

---
+++ rooms/management/commands/seed_amenities.py
@@ -0,0 +1,60 @@
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+
+    help = "This command tells me that he loves me"
+
+    def add_arguments(self, parser):
+        """ parser.add_argument(
+            "--times", help="How many times do you want me to tell you that I love you?"
+        ) """
+
+        amenities = [
+            "Air conditioning",
+            "Alarm Clock",
+            "Balcony",
+            "Bathroom",
+            "Bathtub",
+            "Bed Linen",
+            "Boating",
+            "Cable TV",
+            "Carbon monoxide detectors",
+            "Chairs",
+            "Children Area",
+            "Coffee Maker in Room",
+            "Cooking hob",
+            "Cookware & Kitchen Utensils",
+            "Dishwasher",
+            "Double bed",
+            "En suite bathroom",
+            "Free Parking",
+            "Free Wireless Internet",
+            "Freezer",
+            "Fridge / Freezer",
+            "Golf",
+            "Hair Dryer",
+            "Heating",
+            "Hot tub",
+            "Indoor Pool",
+            "Ironing Board",
+            "Microwave",
+            "Outdoor Pool",
+            "Outdoor Tennis",
+            "Oven",
+            "Queen size bed",
+            "Restaurant",
+            "Shopping Mall",
+            "Shower",
+            "Smoke detectors",
+            "Sofa",
+            "Stereo",
+            "Swimming pool",
+            "Toilet",
+            "Towels",
+            "TV",
+        ]
+
+    def handle(self, *args, **options):
+        times = options.get("times")
+        for t in range(0, int(times)):
+            self.stdout.write(self.style.SUCCESS("I love you"))
