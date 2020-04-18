e8bf9ffae8751c031430f90b6bcdc5b92691a3a0
vapvin <tpehdj12@daum.net>
2020-04-18 11:13:10 +0900

    Rovins Project Endpoint Template

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,9 @@
+from datetime import datetime
 from django.shortcuts import render
+from . import models
 
 
-def all_rooms():
-    pass
+def all_rooms(request):
+    all_rooms = models.Room.objects.all()
+    return render(request, "rooms/all_rooms.html", context={"rooms": all_rooms})
+
