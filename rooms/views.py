ce7a1d694f140605d6bf128a44f3decd8bbaff0f
vapvin <tpehdj12@daum.net>
2020-04-25 01:43:24 +0900

    Delete

--- rooms/views.py
+++ rooms/views.py
@@ -12,9 +12,3 @@ class HomeView(ListView):
     paginate_orphans = 5
     ordering = "created"
     context_object_name = "rooms"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        now = timezone.now()
-        context["now"] = now
-        return context
